3efd0a8123fa4e0991d05be250c10cbf2d554941
Saad-Hussain31 <saadnasir31@gmail.com>
2021-05-16 15:01:01 +0500

    templates added

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse (got rid of this import coz I
+# am no longer returning HttpsResponse, but render template 
 
+#pretend that we made a database call and got back this 'posts' list
+#We wanna display this post on bloghome
+posts = [
+   {
+        'author': 'Saad Hussain',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2021'    
+   },
+   {
+        'author': 'Tolstoy',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2021'    
+   }
 
+]
 
 # logic to handle different routes goes in this function
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
-
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 #fucntion to make about page
 def about(request):
-    return HttpResponse("<h1>Blog About </h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
+
