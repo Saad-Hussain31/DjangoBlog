755a18726626e9a349b5b2037be6e85c74731b8d
Saad-Hussain31 <saadnasir31@gmail.com>
2021-05-17 22:22:51 +0500

    created Database and models of Posts and Users.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Creating posts model/class
+class  Post(models.Model): #each class is going to be its own table in DB
+    title = models.CharField(max_length=100) #field/attribute
+    content = models.TextField() #unrestricted coz it could be big
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render
 # from django.http import HttpResponse (got rid of this import coz I
 # am no longer returning HttpsResponse, but render template 
-
+from .models import Post #why .? watch video 5 28:00
 #pretend that we made a database call and got back this 'posts' list
 #We wanna display this post on bloghome
-posts = [
-   {
-        'author': 'Saad Hussain',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2021'    
-   },
-   {
-        'author': 'Tolstoy',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2021'    
-   }
-
-]
 
 # logic to handle different routes goes in this function
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 #fucntion to make about page
